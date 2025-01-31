962d32ba7b4ea78688cd1edd8e852ef3c58f60d0
Damien Deerinck <d.f.d@me.com>
2025-01-31 10:22:43 -0800

    button now displays input text on text view

--- app/src/main/java/com/example/assignment1_damiendeerinck_toniharo/MainActivity.kt
+++ app/src/main/java/com/example/assignment1_damiendeerinck_toniharo/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.assignment1_damiendeerinck_toniharo
 
+
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val findGrade = findViewById<Button>(R.id.gradeButton)
+        findGrade.setOnClickListener {
+            val gradeIn = findViewById<TextInputEditText>(R.id.gradeInput)
+            val numberGrade = gradeIn.text
+            val output = findViewById<TextView>(R.id.gradeView)
+            output.text = numberGrade
+        }
     }
+
 }
