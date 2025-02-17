fbe3e6aac591c758a6f6e26848eb56535628c208
Damien Deerinck <d.f.d@me.com>
2025-02-17 14:02:17 -0800

    Final update

--- app/src/main/java/com/example/assignment1_damiendeerinck_toniharo/MainActivity.kt
+++ app/src/main/java/com/example/assignment1_damiendeerinck_toniharo/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.assignment1_damiendeerinck_toniharo
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,35 +25,9 @@ class MainActivity : AppCompatActivity() {
 
 
         val gradeScreenButton = findViewById<Button>(R.id.toGradeScreen)
-        gradeScreenButton.setOnClickListener{
+        gradeScreenButton.setOnClickListener {
             val intent = Intent(this, GradeActivity::class.java)
             startActivity(intent)
         }
-        /*
-        val findGrade = findViewById<Button>(R.id.gradeButton)
-        findGrade.setOnClickListener {
-            val gradeIn = findViewById<TextInputEditText>(R.id.gradeInput)
-            val numberGrade = gradeIn.text
-            val output = findViewById<TextView>(R.id.gradeView)
-            output.text = getGrade(numberGrade.toString())
-        }*/
-
-
     }
-
-    /*
-    fun getGrade(gradeIn:String):String {
-        val score = gradeIn.toDoubleOrNull()
-
-        return if(score == null) {"Null"
-        } else if(score >  94){"A"
-        } else if(score >= 90){"A-"
-        } else if(score >= 87){"B+"
-        } else if(score >= 83){"B"
-        } else if(score >= 80){"B-"
-        } else if(score >= 77){"C+"
-        } else if(score >= 73){"C"
-        } else if(score >= 70){"C-"
-        } else "F"
-    }*/
 }
